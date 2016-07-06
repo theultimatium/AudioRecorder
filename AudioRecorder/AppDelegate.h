7e4b5237d91c8cbc415f32d03f6c3fcb9438d8bc
Julius Nedzinskas <nedjulius@gmail.com>
2016-07-06 17:43:58 +0300

    Initial Commit

---
+++ AudioRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioRecorder
+//
+//  Created by Julius Nedzinskas on 7/6/16.
+//  Copyright © 2016 Julius Nedzinskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecorder
+//
+//  Created by Julius Nedzinskas on 7/6/16.
+//  Copyright © 2016 Julius Nedzinskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
